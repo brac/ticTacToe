d42ba56261518b9b2031f22dfeec28d323c57fbe
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-05-30 15:57:53 -0500

    listening for clicks on gameboard

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// jshint asi:true
+// TODO: Display something when clicked
+//     Alternate players on click on empty space
+//     Dispaly the correct symbol depending on player
+//     Check for win
+//     Flash winning row
+//     Flash tied grid
+//     Reset board, keep score
+
+
 (function () {
-  console.log('yo yo')
+
+  class Gameboard{
+    constructor(element) {
+      this.turnCount = 0
+      this.element = element
+    }
+  }
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const gameboard = new Gameboard(document.querySelector('.gameboard'))
+
+    gameboard.element.addEventListener('click', (event) => {
+      console.log(event.target)
+
+    })
+  })
 })()
